2da8576ea7416f90e11aaf7a4c61d1798e9611aa
phaninandigam <phaniraghavendra.namdigam@gmail.com>
2025-01-04 21:29:00 +0530

    Update runApplication.groovy

--- vars/runApplication.groovy
+++ vars/runApplication.groovy
@@ -1,7 +1,7 @@
 def call() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
-    sleep(time: 15, unit: 'SECONDS')
+    sleep(time: 120, unit: 'SECONDS')
 
     def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${publicIp}:8080"
