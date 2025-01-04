5e4b451697502627acf3093a21a450b280eb557e
phaninandigam <phaniraghavendra.namdigam@gmail.com>
2025-01-04 21:43:07 +0530

    Update checkout_build_run.groovy

--- vars/checkout_build_run.groovy
+++ vars/checkout_build_run.groovy
@@ -1,1 +1,20 @@
-test
+def checkout() {
+    echo 'Checking out code...'
+    checkout scm
+}
+
+def build() {
+    echo 'Building project with Maven...'
+    sh 'mvn clean package'
+}
+
+def run() {
+    echo 'Running Spring Boot application...'
+    sh 'nohup mvn spring-boot:run &'
+
+    def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
+    echo "The application is running and accessible at: http://${publicIp}:8080"
+    sleep(time: 60, unit: 'SECONDS')
+
+   
+}
