9e771821bc88c6a3cdb4e449c2502ef7158c0985
phaninandigam <phaniraghavendra.namdigam@gmail.com>
2025-01-04 21:32:48 +0530

    Update runApplication.groovy

--- vars/runApplication.groovy
+++ vars/runApplication.groovy
@@ -4,7 +4,7 @@ def call() {
 
     def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${publicIp}:8080"
-    sleep(time: 120, unit: 'SECONDS')
+    sleep(time: 60, unit: 'SECONDS')
 
    
 }
